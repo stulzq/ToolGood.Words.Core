a6b85c66bfe04ac023c92d2fe829237bb7a6768e
stulzq <stulzq@qq.com>
2017-08-18 15:50:08 +0800

    Init Commit

---
+++ ToolGood.Words.Core/internals/BaseSearch.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace ToolGood.Words.Core.internals
+{
+	public abstract class BaseSearch
+	{
+		protected TrieNode[] _first = new TrieNode[char.MaxValue + 1];
+		protected TrieNode _root = new TrieNode();
+
+
+		public virtual void SetKeywords(ICollection<string> _keywords)
+		{
+			var first = new TrieNode[char.MaxValue + 1];
+			var root = new TrieNode();
+
+			foreach (var p in _keywords)
+			{
+				if (string.IsNullOrEmpty(p)) continue;
+
+				//var nd = root;
+				var nd = _first[p[0]];
+				if (nd == null)
+				{
+					nd = root.Add(p[0]);
+					first[p[0]] = nd;
+				}
+				for (var i = 1; i < p.Length; i++) nd = nd.Add(p[i]);
+				nd.SetResults(p);
+			}
+			_first = first; // root.ToArray();
+
+			var links = new Dictionary<TrieNode, TrieNode>();
+			foreach (var item in root.m_values) TryLinks(item.Value, null, links);
+
+			foreach (var item in links) item.Key.Merge(item.Value);
+
+			_root = root;
+		}
+
+		private void TryLinks(TrieNode node, TrieNode node2, Dictionary<TrieNode, TrieNode> links)
+		{
+			foreach (var item in node.m_values)
+			{
+				TrieNode tn = null;
+				if (node2 == null)
+				{
+					tn = _first[item.Key];
+					if (tn != null) links[item.Value] = tn;
+				}
+				else
+				{
+					if (node2.TryGetValue(item.Key, out tn)) links[item.Value] = tn;
+				}
+				TryLinks(item.Value, tn, links);
+			}
+		}
+	}
+}
---
+++ ToolGood.Words.Core/internals/TrieNode.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+
+namespace ToolGood.Words.Core.internals
+{
+    public class TrieNode
+    {
+        //public byte Type { get; set; }
+        public bool End { get; set; }
+        public List<string> Results { get; private set; }
+        internal Dictionary<char, TrieNode> m_values;
+        private uint minflag = uint.MaxValue;
+        private uint maxflag = uint.MinValue;
+
+
+        public TrieNode()
+        {
+            m_values = new Dictionary<char, TrieNode>();
+            Results = new List<string>();
+        }
+
+        public bool TryGetValue(char c, out TrieNode node)
+        {
+            if (minflag <= (uint)c && maxflag >= (uint)c) {
+                return m_values.TryGetValue(c, out node);
+            }
+            node = null;
+            return false;
+        }
+        public ICollection<TrieNode> Transitions()
+        {
+            return m_values.Values;
+        }
+
+        public TrieNode Add(char c)
+        {
+            TrieNode node;
+
+            if (m_values.TryGetValue(c, out node)) {
+                return node;
+            }
+
+            if (minflag > c) { minflag = c; }
+            if (maxflag < c) { maxflag = c; }
+
+            node = new TrieNode();
+            m_values[c] = node;
+            //m_values.Add(c, node);
+            return node;
+        }
+
+        public void SetResults(string text)
+        {
+            if (End == false) {
+                End = true;
+            }
+            Results.Add(text);
+        }
+
+        public void Merge(TrieNode node)
+        {
+            if (node.End) {
+                if (End == false) {
+                    End = true;
+                }
+                foreach (var item in node.Results) {
+                    Results.Add(item);
+                }
+            }
+
+            foreach (var item in node.m_values) {
+                //if (minflag > (uint)item.Key) {
+                //    minflag = (uint)item.Key;
+                //    m_values[item.Key] = item.Value;
+                //} else if (maxflag < (uint)item.Key) {
+                //    maxflag = (uint)item.Key;
+                //    m_values[item.Key] = item.Value;
+                //} else if (m_values.ContainsKey(item.Key) == false)  {
+                //    m_values[item.Key] = item.Value;
+                //}
+
+                if ( m_values.ContainsKey(item.Key) == false) {
+                    if (minflag > item.Key) { minflag = item.Key; }
+                    if (maxflag < item.Key) { maxflag = item.Key; }
+                    m_values[item.Key] = item.Value;
+                    //m_values.Add(item.Key, item.Value);
+                }
+            }
+        }
+
+        public TrieNode[] ToArray()
+        {
+            TrieNode[] first = new TrieNode[char.MaxValue + 1];
+            foreach (var item in m_values) {
+                first[item.Key] = item.Value;
+            }
+            return first;
+        }
+
+    }
+
+}
